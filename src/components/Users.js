dc93b56beff21d847b0e62e2642e2684359a65de
jbrasko <jbrasko0508@gmail.com>
2021-03-10 14:47:27 -0600

    commit tree e8e575cefdb2e9a3d968f8a9aefad4bad5a10cc1

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 // add any needed imports here
+// class Users extends Component {
+
+//   render() {
+//     return (
+//       <div>
+//         <ul>
+//           Users!
+//           {/* Write code here that displays the usernames of all users in the Redux store */}
+//           {this.props.users.map(user => {
+//             return <li key={user.username}>{user.username}</li>
+//           })}
+//           {/* In addition, display the total number of users curently in the store */}
+//         </ul>
+//         <p>{this.props.userCount}</p>
+//       </div>
+//     )
+//   }
+// }
+
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>);
+
     return (
       <div>
+        {this.props.userCount}
         <ul>
-          Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {users}
         </ul>
       </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return {users: state.users, userCount: state.users.length}
+}
+
 //add mapStateToProps here
 
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users)
